1cbdcdc66d9404cb80f9126e70fc6598a7a4f34b
NurKeinNeid <mralexman3000@gmail.com>
2024-04-07 20:47:56 +0200

    prebuilts: Switch to Accord as music player [1/2]
    Change-Id: I22c2ad6b6da0aae9385631f7f01a1eae7eb59d77
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- apps/Android.mk
+++ apps/Android.mk
@@ -1,11 +1,11 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := Gramophone
+LOCAL_MODULE := Accord
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
 LOCAL_CERTIFICATE := PRESIGNED
-LOCAL_SRC_FILES := gramophone/gramophone.apk
+LOCAL_SRC_FILES := accord/accord.apk
 LOCAL_PRODUCT_MODULE := true
 LOCAL_OPTIONAL_USES_LIBRARIES := androidx.window.extensions androidx.window.sidecar
 include $(BUILD_PREBUILT)
